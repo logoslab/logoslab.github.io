fa07c34fd9dd3a89fea14e42eeb24cfcdeeb4b7a
suvincent <suvincent0226@gmail.com>
2022-12-16 15:12:12 +0800

    update research area

--- pages/components/researchArea.tsx
+++ pages/components/researchArea.tsx
@@ -7,12 +7,51 @@ import styles from '../../styles/Home.module.css'
 const ResearchArea: NextPage = () => {
   return (
     <Container id={'Research_Area'}>
-      <br/><br/><br/>
+      <br /><br /><br />
       <h2>ResearchArea</h2>
       <div>
-      <ul><li><strong>Blockchain core technology and applications</strong><ul><li>Consensus algorithm</li><li>Multi-threaded chaining</li><li>FinTech – automatic auditing system</li><li>E-Voting system</li><li>Hardware eWallet</li></ul></li><li><strong>Distributed Secure IoT framework</strong><ul><li>Self-sovereign eId</li><li>An ultra-robust private key management system</li><li>Self-sovereign access control protocol</li><li>Peer-to-peer transaction model</li><li>Trusted computing environment</li><li>Privacy-preserving techniques</li></ul></li><li><strong>Dafuon Entrepreneurship education system</strong><ul><li>Gamified education system</li><li>Resource map and scheduling</li><li>Personal talent exploration and team formation</li></ul></li><li><strong>Edge AI technology</strong></li><li><strong>Entrepreneurship learning map </strong></li></ul>
-      {/* <h4><strong>The most creative, productive and exciting research lab …</strong></h4> */}
-		</div>
+        <ul>
+          <li>
+            <strong>Blockchain Core Technology and Applications</strong>
+            <ul>
+              <li>Consensus algorithm</li>
+              <li>Multi-threaded chaining</li>
+              <li>FinTech - automatic auditing system</li>
+              <li>E-Voting system</li>
+            </ul>
+          </li>
+          <li>
+            <strong>Distributed Secure IoT framework</strong>
+            <ul>
+              <li>Self-sovereign eId</li>
+              <li>An ultra-robust private key management system</li>
+              <li>Self-sovereign access control protocol</li>
+              <li>Peer-to-peer transaction model</li>
+              <li>Privacy-preserving techniques</li>
+            </ul>
+          </li>
+          <li>
+            <strong>Trusted Computing and Hardware Security Chip</strong>
+            <ul>
+              <li>Trusted computing environment</li>
+              <li>Secure element</li>
+              <li>Hardware eWallet</li>
+            </ul>
+          </li>
+          <li>
+            <strong>Dafuon Entrepreneurship education system</strong>
+            <ul>
+              <li>Gamified education system</li>
+              <li>Resource map and scheduling</li>
+              <li>Personal talent exploration and team formation</li>
+            </ul>
+          </li>
+          <li>
+            <strong>Entrepreneurship learning map </strong>
+          </li>
+        </ul>
+        {/* <h4><strong>The most creative, productive and exciting research lab …</strong></h4> */}
+      </div>
     </Container>
   )
 }
